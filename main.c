d5c0e23f2b5a72c9ddf87b3ca4d34a6d82e38b30
KAKAR Ahmad-Bilal <Ahmad-Bilal.KAKAR@cpnv.ch>
2019-03-07 11:07:49 +0100

    add recursive function

--- main.c
+++ main.c
@@ -3,21 +3,17 @@
 
 int main()
 {
-    int number=0;
-    printf("Please enter a number: ");
-    scanf("%i",&number);
-    factoriel(number); //some comment
-    return 0; //jkjf
+   int fact =5;
+   printf("%d! = %d \n", fact, factoriel(fact));
+    return 0;
 }
 
-void factoriel(int fact)
+int factoriel(int f)
 {
     // add a comment
-    int total=fact;
-    for (int i=fact-1; i>0; i--)
+    if(f == 1)
     {
-        total=total * i;
+        return 1;
     }
-    printf("%d! = %d \n", fact, total);
-    return 0;
+    return (f*factoriel(f-1));
 }
